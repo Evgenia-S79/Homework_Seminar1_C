a8845270c7514e22679b7c8f52fd63b6c0c0c89f
Evgenia S <Dla-evgenii@yandex.ru>
2022-10-06 22:32:30 +0600

    Задача 2 (нахождение большего и меньшего числа из двух введенных)

---
+++ task_two/Program.cs
@@ -0,0 +1,15 @@
+﻿// напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine("{0} Большее число", a);
+    Console.WriteLine("{0} Мельшее число", b);
+}
+else
+{
+    Console.WriteLine("{0} Большее число", b);
+    Console.WriteLine("{0} Мельшее число", a);
+}
